cebb5749be5a099e4675159aedd04a9c5df0d6d7
KronosTheLate <densbal@live.no>
2021-06-09 17:39:17 +0200

    More julian internals
    Missing the addition of a show method for ESeries objects.

---
+++ src/Utilities.jl
@@ -0,0 +1,22 @@
+geometric_mean(a::Number, b::Number) = √(a*b)
+
+function identify_series_candidates(a::Number, possible_values::Array)
+    i, j = 1, 2
+        while !(possible_values[i] ≤ a ≤ possible_values[j])
+            i+=1; j+=1
+        end
+    return possible_values[i], possible_values[j]
+end
+
+function norm_to_between_100_and_1000(val::Number)
+    power_of_10 = 0.0
+    while val > 1000
+        val/=10; power_of_10 += 1
+    end
+    while val ≤ 100
+        val*=10; power_of_10 += -1
+    end
+    return val, power_of_10
+end
+
+nothing
