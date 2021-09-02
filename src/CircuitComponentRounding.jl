b8319aa3acf5c65cb672f4105e35e772138696f9
KronosTheLate <61620837+KronosTheLate@users.noreply.github.com>
2021-09-02 09:17:11 +0200

    Added dependency on ESeriesRounding

--- src/CircuitComponentRounding.jl
+++ src/CircuitComponentRounding.jl
@@ -1,14 +1,9 @@
 module CircuitComponentRounding
 
-using NumericIO: formatted
-import Base: round, print, show
+import ESeriesRounding: E3, E6, E12, E24, E48, E96, E192
+import ESeriesRounding: determine_E
 
 export E3, E6, E12, E24, E48, E96, E192
 export determine_E
-
-abstract type AbstractPrefNumbSys end
-
-include("ESeries.jl")
-include("Utilities.jl")
 
 end
